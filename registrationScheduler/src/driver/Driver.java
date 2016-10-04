f3d981f5418668deb760567938c4286fce9a253f
Shreyas MN <shreyaschinni@gmail.com>
2016-10-04 15:08:07 -0400

    Update Driver.java

--- registrationScheduler/src/driver/Driver.java
+++ registrationScheduler/src/driver/Driver.java
@@ -1,20 +1,22 @@
-package src.driver;
-import java.io.File;
-import src.util.Logger;
-import src.store.FileDsiplayInterface;
-import src.store.Results;
-import src.store.StdOutDisplayInterface;
-import src.util.Scheduler;
-import src.util.FileProcessor;
-import src.threadMgmt.CreateWorkers;
+package registrationScheduler.driver;
 
-import static src.util.Constants.INPUT_FILE_INDEX;
-import static src.util.Constants.IN_THREADS_INDEX;
-import static src.util.Constants.MINIMUM_THREADS;
-import static src.util.Constants.MAXIMUM_THREADS;
-import static src.util.Constants.MIN_DEBUG_LEVEL;
-import static src.util.Constants.MAX_DEBUG_LEVEL;
-import static src.util.Constants.IN_DEBUG_LEVEL_INDEX;
+import java.io.File;
+import registrationScheduler.util.Logger;
+import registrationScheduler.store.FileDsiplayInterface;
+import registrationScheduler.store.Results;
+import registrationScheduler.store.StdOutDisplayInterface;
+import registrationScheduler.util.Scheduler;
+import registrationScheduler.util.FileProcessor;
+import registrationScheduler.threadMgmt.CreateWorkers;
+
+import static registrationScheduler.util.Constants.INPUT_FILE_INDEX;
+import static registrationScheduler.util.Constants.OUTPUT_FILE_INDEX;
+import static registrationScheduler.util.Constants.IN_THREADS_INDEX;
+import static registrationScheduler.util.Constants.MINIMUM_THREADS;
+import static registrationScheduler.util.Constants.MAXIMUM_THREADS;
+import static registrationScheduler.util.Constants.MIN_DEBUG_LEVEL;
+import static registrationScheduler.util.Constants.MAX_DEBUG_LEVEL;
+import static registrationScheduler.util.Constants.IN_DEBUG_LEVEL_INDEX;
 
 /**
  * @author ${Shreyas Mahanthappa Nagaraj}
@@ -24,23 +26,17 @@ public class Driver {
 	
 	public static void main(String args[]) {
 		Driver driver = new Driver();
-//		boolean valid = driver.validateInputArguements(args);
-		boolean valid = true;
+		boolean valid = driver.validateInputArguements(args);
 		if (valid) {
-//			boolean validFiles = driver.validateFiles(args);
-			boolean validFile = true;
+			boolean validFile = driver.validateInputFile(args);
 			  if (validFile) {
-//				Logger.setDebugLevel(Integer.parseInt(args[IN_DEBUG_LEVEL_INDEX]));
-				Logger.setDebugLevel(2);
-//				String inputfile = args[INPUT_FILE_INDEX];
-				String inputfile = "input.txt";
-//				int numthreads = Integer.parseInt(args[IN_THREADS_INDEX]);
-				int numthreads = 1;
+				Logger.setDebugLevel(Integer.parseInt(args[IN_DEBUG_LEVEL_INDEX]));
+				String inputfile = args[INPUT_FILE_INDEX];
+				int numthreads = Integer.parseInt(args[IN_THREADS_INDEX]);
 				
 				FileProcessor fileprocessor = new FileProcessor(inputfile);
 				Scheduler assigncourses = new Scheduler();
-//				Results resultsInterface = new Results(args[OUTPUT_FILE_INDEX]);
-				FileDsiplayInterface fdinterface = new Results("output.txt");
+				FileDsiplayInterface fdinterface = new Results(args[OUTPUT_FILE_INDEX]);
 				StdOutDisplayInterface stdoutInterface = new Results();
 				
 				CreateWorkers createWorkers = new CreateWorkers(fileprocessor, assigncourses, stdoutInterface,fdinterface);
@@ -60,20 +56,11 @@ public class Driver {
 	 * @return whether file is valid
 	 * 
 	 */
-	private boolean validateFiles(String[] args) {
+	private boolean validateInputFile(String[] args) {
 		System.out.println("file name :"+args[INPUT_FILE_INDEX]);
-		File inFile = new File(args[INPUT_FILE_INDEX]);
+		File file = new File(args[INPUT_FILE_INDEX]);
 		boolean res = true;
-		
-		String outFile = args[1];
-		if(!outFile.endsWith(".txt")){
-			res = false;
-			System.err.println("Invalid Output File name");
-			System.exit(1);
-		}
-		
-		if(!inFile.exists() || inFile.length()==0L){
-			res = false;
+		if(!file.exists() || file.length()==0L){
 			System.err.println("Empty file or Input File not present");
 			System.exit(1);
 		}
